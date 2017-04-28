1a8a36b3e9885b4d616afc099869be46216926e2
karatekid <mchristen96@gmail.com>
2017-04-28 08:59:35 -0700

    Use meaningful coverage and make serializer

--- repo_monitor/parse_test_output.py
+++ repo_monitor/parse_test_output.py
@@ -10,18 +10,21 @@ def get_total_coverage(filename='coverage.xml'):
 
 
 NosetestData = namedtuple('NosetestData', ['num_tests', 'time', 'test2time'])
+class NosetestData(object):
+    def __init__(self, filename='test-output/nosetests.xml'):
+        tree = ET.parse(filename)
+        root = tree.getroot()
+        assert root.tag == 'testsuite'
+        self.time = float(root.attrib['time'])
+        self.num_tests = int(root.attrib['tests'])
+        self.test2time = {}
+        for test_case in root:
+            assert test_case.tag == 'testcase'
+            test_name = '{}:{}'.format(
+                test_case.attrib['classname'], test_case.attrib['name'])
+            self.test2time[test_name] = float(test_case.attrib['time'])
+        assert self.num_tests == len(self.test2time)
 
 
 def parse_nosetest_output(filename='test-output/nosetests.xml'):
-    tree = ET.parse(filename)
-    root = tree.getroot()
-    assert root.tag == 'testsuite'
-    time = float(root.attrib['time'])
-    num_tests = int(root.attrib['tests'])
-    test2time = {}
-    for test_case in root:
-        assert test_case.tag == 'testcase'
-        test_name = '{}:{}'.format(
-            test_case.attrib['classname'], test_case.attrib['name'])
-        test2time[test_name] = float(test_case.attrib['time'])
-    return NosetestData(time=time, num_tests=num_tests, test2time=test2time)
+    return NosetestData(filename=filename)
