31edca14f0484d030ec107b182082ad36be15b48
karatekid <mchristen96@gmail.com>
2017-04-29 16:55:05 -0700

    Import __version__ in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import find_packages
 from setuptools import setup
 
+import repo_monitor
+
 
 PACKAGE_NAME = 'repo_monitor'
 
 
 setup(
     name=PACKAGE_NAME,
-    version='0.1.0',
+    version=repo_monitor.__version__,
     description='Utility for monitoring certain aspects of a repo',
     author='Michael Christen',
     url='https://github.com/michael-christen/repo-monitor',
